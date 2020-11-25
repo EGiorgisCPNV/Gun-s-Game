5342062b160fb9f752213afde5a8160495c2366c
GIORGIS Esteban <Esteban.GIORGIS@cpnv.ch>
2020-11-25 08:40:58 +0100

    Mise en ordre du code

--- Projet_CSharpe/Assets/Scripts/PersonnageDeplacement.cs
+++ Projet_CSharpe/Assets/Scripts/PersonnageDeplacement.cs
@@ -15,7 +15,6 @@ public class PersonnageDeplacement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         /* COMPRENDRE transform
          si il y a transform.position = Vector3.zero
         alors au lancement du jeu le personnage ira a la position (0, 0, 0)
@@ -25,8 +24,15 @@ public class PersonnageDeplacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        toMove();
+        toJump();
+        toCrouch();
+        toSprint();
+        GetGravity();        
+    }
 
+    private void toMove()
+    {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
@@ -34,32 +40,57 @@ public class PersonnageDeplacement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
 
+    }
+    
 
+    //Methode pour pouvoir sauter
+    private void toJump()
+    {
         //saut
         if (Input.GetKeyDown(KeyCode.Space))
         {
             velocity.y = jump;
         }
+    }
 
 
+    //Methode pour pouvoir s'acroupir
+    private void toCrouch()
+    {
+        //s'acroupir
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            controller.height /= 2;
+        }
+        //fin de s'acroupire
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            controller.height *= 2;
+        }
+    }
+
+    //Methode pour pouvoir courir
+    private void toSprint()
+    {
         //courir 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            speed = speed * 2;           
+            speed = speed * 2;
         }
         //fin de courir
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             speed = speed / 2;
         }
+    }
 
-
-
+    //Methode pour la gravité
+    private void GetGravity()
+    {
         //Gravité
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
-
-
     }
+
+
 }
