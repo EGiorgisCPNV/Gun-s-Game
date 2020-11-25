e565e8682679bb06899f7ce426ce721ec3b1585c
Dylan Rocha <dylan-david.cunha-rocha@cpnv.ch>
2020-11-25 09:20:52 +0100

    tir avec arme sans animation

---
+++ Projet_CSharpe/Assets/Scripts/Script gun/Targget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Targget : MonoBehaviour
+{
+    public float heatlh = 10f;
+
+    public void TakeDamage(float amount)
+    {
+        heatlh -= amount;
+        if (heatlh <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
