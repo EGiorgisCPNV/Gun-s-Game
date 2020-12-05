789ac875da88dcb4913ea9b2af417b409dff60bf
GIORGIS Esteban <Esteban.GIORGIS@cpnv.ch>
2020-12-05 11:45:54 +0100

    Shot amelioration

--- Projet_CSharpe/Assets/Scripts/Script gun/BulletHole.cs
+++ Projet_CSharpe/Assets/Scripts/Script gun/BulletHole.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BulletHole : MonoBehaviour
 {
 
     //Public//
     public GameObject bulletHole ;
     public float distance = 1000f;
-    Vector3 randomPosition;
+    
 
-     
     //Private//
     Camera cam;
-
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +24,15 @@ public class BulletHole : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log("BulletHole " + takeMethode.RandomPosition);
 
 
         if (Input.GetButtonDown("Fire1"))
         {
             RaycastHit hit;
 
-            if(Physics.Raycast(cam.transform.position, RandomPosition, out hit, distance))
+
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, distance))
             {             
                 Targget target = hit.transform.GetComponent<Targget>();
                 if (target.heatlh > 0)
--- Projet_CSharpe/Assets/Scripts/Script gun/gunsScript.cs
+++ Projet_CSharpe/Assets/Scripts/Script gun/gunsScript.cs
@@ -6,47 +6,32 @@ using System.Windows.Input;
 public class gunsScript : MonoBehaviour
 {
 
+
+
     //Public//
-    public float damage = 10f;
     public float maxRange = 100f;
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impact;
     public float fireRate = 15f;
-    Vector3 randomPosition;
+
 
     //Private//
+    private float damage = 10f;
     private float nextTimeToFire = 0f;
     AudioSource shootSound;
+    Vector3 randomPosition;
 
-    //cette methode est appelée au lancement du programe 
-    private void Start()
+
+
+
+
+
+    //cette accesseur sert a retourner la valeur des dégats
+    public float Damage
     {
-        shootSound = GetComponent<AudioSource>();
-        
-
+        get { return damage; }
     }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        //ne peut pas être initialiser en haut dans les attribut, que dans des methode
-        randomPosition = new Vector3(Random.Range(0, 10), Random.Range(0, 10), 100);
-
-        //Debug.Log(randomPosition);
-
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
-            {
-               nextTimeToFire = Time.time + 1f / fireRate;
-               shootSound.Play();
-               Shoot();
-            }
-
-
-        
-    }
-
 
 
     public Vector3 RandomPosition
@@ -57,60 +42,100 @@ public class gunsScript : MonoBehaviour
         }
     }
 
-    //cette accesseur sert a retourner la valeur des dégats
-    public float Damage
+    //cette methode est appelée au lancement du programe 
+    private void Start()
     {
-        get { return damage; }
+
+        shootSound = GetComponent<AudioSource>();
     }
 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Random.Range(100f, 100f)
+
+        //ne peut pas être initialiser en haut dans les attribut, que dans des methode
+        randomPosition = new Vector3(0.1f, 0, 33f);
+
+        //Debug.Log(randomPosition);
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+            {
+               nextTimeToFire = Time.time + 1f / fireRate;
+               shootSound.Play();
+               Shoot();
+            
+        }
+
+
+
+    }
+
+    
 
     //cette methode permet 
     void Shoot()
     {
-        tireAssit();
-        tirStatique();
+        if (Input.GetButton("Space"))
+        {
+            Debug.Log("JumpShot");
+            Debug.Log(randomPosition);
+            JumpShot();
+        }
+        else
+        {
+            Debug.Log("StaticShot");
+            Debug.Log(randomPosition);
+            StaticShot();
+        }
+        
+       
     }
 
 
-    private void tireAssit()
+    private void JumpShot()
     {
         muzzleFlash.Play();//affiche l'effet
 
-        //transform.TransformDirection(position)
 
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, randomPosition, out hit, maxRange) && Input.GetButtonDown("Space"))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, maxRange))
         {
-            Debug.Log(hit.transform.name);
+            //Debug.Log(hit.transform.name);
+
 
             Targget target = hit.transform.GetComponent<Targget>();
             if (target != null)
             {
                 target.TakeDamage(damage);
             }
+                        
 
             GameObject bulletEffect = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(bulletEffect, 1f);
+
+
         }
     }
 
 
-    private void tirStatique()
+    private void StaticShot()
     {
         muzzleFlash.Play();//affiche l'effet
-
-        //transform.TransformDirection(position)
+        
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward , out hit, maxRange))
         {
-            Debug.Log(hit.transform.name);
+            //Debug.Log(hit.transform.name);
 
             Targget target = hit.transform.GetComponent<Targget>();
             if (target != null)
             {
                 target.TakeDamage(damage);
             }
+            
 
             GameObject bulletEffect = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(bulletEffect, 1f);
