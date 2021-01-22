4e9b723f32813349e12a1d71865c47239b7c52b5
Dylan Rocha <dylan-david.cunha-rocha@cpnv.ch>
2021-01-22 10:47:21 +0100

    fps player arms and map added

--- Projet_CSharpe/Assets/Animations/Game Kit Controller/Scripts/PickUps/pickUpManager.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-public class pickUpManager : MonoBehaviour {
-	public List<pickUpElementInfo> mainPickUpList = new List<pickUpElementInfo> ();
-}
